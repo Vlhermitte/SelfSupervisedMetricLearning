4bff296548ca1161846f311e0c265ddb64bdc8c4
Vlhermitte <valentin.lhermitte2604@gmail.com>
2024-06-08 09:58:44 +0200

    Add resnet backbone

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision.models import resnet18
+from torchvision.models import ResNet18_Weights
 
 # Define the model (CNN)
 class BloodNet(nn.Module):
@@ -9,25 +11,19 @@ class BloodNet(nn.Module):
     """
     def __init__(self, use_max=True):
         super(BloodNet, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-        )
-        # Use either max or average pooling
-        self.pool = nn.AdaptiveMaxPool2d((1, 1)) if use_max else nn.AdaptiveAvgPool2d((1, 1))
+        self.resnet = resnet18(weights=ResNet18_Weights.DEFAULT)
+        self.resnet = nn.Sequential(*list(self.resnet.children())[:-2])
+        self.pool = nn.AdaptiveMaxPool2d(1) if use_max else nn.AdaptiveAvgPool2d(1)
 
     def forward(self, x, eps=1e-6):
-        x = self.conv(x)
+        x = self.resnet(x)
         x = self.pool(x)
-        x = F.normalize(x, p=2, dim=1, eps=eps)
         x = x.view(x.size(0), -1)
+        x = F.normalize(x, p=2, dim=1, eps=eps)
         return x
+
+    def __str__(self):
+        return "BloodNet"
 
 
 class SiameseNetwork(nn.Module):
@@ -63,3 +59,6 @@ class SiameseNetwork(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
+
+    def __str__(self):
+        return "SiameseNetwork"
