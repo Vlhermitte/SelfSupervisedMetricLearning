07a1b53e0d36c89041751ead89d9577631dbf3f5
Vlhermitte <valentin.lhermitte2604@gmail.com>
2024-06-10 20:01:24 +0200

    Adding Contrastive loss

--- loss.py
+++ loss.py
@@ -17,3 +17,27 @@ class TripletLoss(nn.Module):
         d_neg = F.pairwise_distance(anchor, negative, p=2)
         loss = F.relu(d_pos - d_neg + self.margin)
         return loss.mean()
+
+
+# Define the loss function (Contrastive loss)
+class ContrastiveLoss(nn.Module):
+    """
+    Contrastive loss (SimCLR)
+    """
+    def __init__(self, margin=1.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, z_i, z_j, temperature=0.5):
+        N = len(z_i)
+        z = torch.cat([z_i, z_j], dim=0)
+        sim = F.cosine_similarity(z.unsqueeze(1), z.unsqueeze(0), dim=2)
+        sim_i_j = torch.diag(sim, N)
+        sim_j_i = torch.diag(sim, -N)
+        positive = torch.cat((sim_i_j, sim_j_i), dim=0).reshape(2*N, 1)
+        negative = sim[torch.ones(2*N, dtype=torch.bool)].reshape(2*N, -1)
+        logits = torch.cat((positive, negative), dim=1)
+        logits /= temperature
+        labels = torch.zeros(2*N, dtype=torch.long, device=z.device)
+        loss = F.cross_entropy(logits, labels)
+        return loss
